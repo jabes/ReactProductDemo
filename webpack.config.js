722dae4991543366e1a5740173ecd3ccc2d697fe
Justin Bull <bull.justin@gmail.com>
2016-10-16 21:47:52 -0700

    Refine webpack config and add babelrc'

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isDev = nodeEnv !== 'production';
+
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const srcPath = path.resolve(__dirname, 'src');
 const distPath = path.resolve(__dirname, 'dist');
@@ -12,40 +15,30 @@ const excludePaths = [
   publicPath
 ];
 
-module.exports = {
+var config = {
   entry: [
-    path.resolve(srcPath, 'index.js')
+    path.resolve(srcPath, 'index')
   ],
   output: {
-    path: path.resolve(distPath),
+    path: distPath,
     publicPath: '/dist/',
     filename: 'bundle.js'
   },
   module: {
     preLoaders: [
       {test: /\.js$/, loader: 'eslint'},
-      {test: /\.json$/, loader: 'json'},
     ],
     loaders: [
       {test: /\.js$/, loader: 'babel', exclude: excludePaths},
+      {test: /\.json$/, loader: 'json'},
       {test: /\.css$/, loader: 'style!css!postcss'},
       {test: /\.scss$/, loader: 'style!css!postcss!sass'}
     ]
   },
-  plugins: [
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false
-    //   }
-    // })
-  ],
+  plugins: [],
   resolve: {
     extensions: ['', '.js', '.scss'],
     root: [srcPath]
-  },
-  babel: {
-    presets: ['es2015', 'react'],
-    plugins: ['transform-runtime']
   },
   postcss: function () {
     return [
@@ -62,3 +55,14 @@ module.exports = {
     contentBase: publicPath
   }
 };
+
+if (!isDev) {
+  config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }));
+}
+
+module.exports = config;
